17b85d5788ae875e2f43741c16248b99aa888bab
Jacob Marra <marraj@wit.edu>
2016-03-02 20:51:07 -0500

    Created new class for gameplay vs level picker

--- src/Level.java
+++ src/Level.java
@@ -4,15 +4,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Dude extends Application{
+public class Level extends Application{
 	
 	public static void main(String[] args){
 		launch(args);
+		
+		
 	}
 	
 	@Override
 	public void start(Stage PrimaryStage) throws Exception {
-		Parent rootLevel = FXMLLoader.load(getClass().getResource("LevelPick.fxml"));
+		Parent rootLevel = FXMLLoader.load(getClass().getResource("Level.fxml"));
 		Scene sceneLevel = new Scene(rootLevel);
 		PrimaryStage.setTitle("Choose A Level");
 		PrimaryStage.setScene(sceneLevel);
