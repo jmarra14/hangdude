a6e6d55bf6fd37e98e77a4b34f485e77c3e96b65
Jacob Marra <marraj@wit.edu>
2016-03-09 23:15:39 -0500

    New GUI. Trying to figure out how to run and code.

--- src/Play.java
+++ src/Play.java
@@ -2,23 +2,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
-public class Level extends Application{
+public class Play extends Application{
+	
 	
 	public static void main(String[] args){
 		launch(args);
-		
-		
 	}
 	
 	@Override
 	public void start(Stage PrimaryStage) throws Exception {
-		Parent rootLevel = FXMLLoader.load(getClass().getResource("Level.fxml"));
-		Scene sceneLevel = new Scene(rootLevel);
-		PrimaryStage.setTitle("Choose A Level");
-		PrimaryStage.setScene(sceneLevel);
+		Parent root = FXMLLoader.load(getClass().getResource("Play.fxml"));
+		Scene scene = new Scene(root);
+		PrimaryStage.setTitle("HangDude");
+		PrimaryStage.setScene(scene);
 		PrimaryStage.show();
-	
 	}
+
 }
